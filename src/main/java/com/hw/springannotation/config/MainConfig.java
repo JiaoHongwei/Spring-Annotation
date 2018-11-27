f920b3ac182f3ed4cb356caf5908bf65c0ee452b
jiaohongwei <jiao.hongwei@qq.com>
2018-11-27 19:43:41 +0800

    5、组件注册-@Scope-设置组件作用域

--- src/main/java/com/hw/springannotation/config/MainConfig.java
+++ src/main/java/com/hw/springannotation/config/MainConfig.java
@@ -2,6 +2,7 @@ package com.hw.springannotation.config;
 
 import com.hw.springannotation.beans.Pension;
 import com.hw.springannotation.service.BookService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -35,6 +36,24 @@ import org.springframework.stereotype.Service;
 )       // 指定只扫描
 public class MainConfig {
 
+    /**
+     * * {@link ConfigurableBeanFactory#SCOPE_SINGLETON SCOPE_SINGLETON}.
+     * * @since 4.2
+     * * @see ConfigurableBeanFactory#SCOPE_PROTOTYPE
+     * * @see ConfigurableBeanFactory#SCOPE_SINGLETON
+     * * @see org.springframework.web.context.WebApplicationContext#SCOPE_REQUEST
+     * * @see org.springframework.web.context.WebApplicationContext#SCOPE_SESSION
+     * * @see #value
+     * <p>
+     * <p>
+     * prototype 多实例的 IOC容器启动的时候不会创建Bean，每次获取的时候才会去创建Bean
+     * singleton 单实例的 默认值 IOC容器启动就会调用方法创建Bean，以后每次就是直接从容器（map.get）中拿
+     * request 同一个请求创建一个实例（WEB环境）
+     * session 同一个session创建一个实例（WEB环境）
+     *
+     * @return
+     */
+    @Scope("prototype") // 多实例
     @Bean(value = "pension01")   // 给容器中注册一个 Bean ；类型为返回值类型，id默认为方法名,
     public Pension pension() {
         return new Pension("hongwei", 24);
